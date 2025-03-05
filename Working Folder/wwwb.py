9af799b9ee359ed5c934d2724a87183dd0fa24f8
beertino <beertino@gmail.com>
2025-03-05 04:53:09 +0000

    add: wwwb data

---
+++ Working Folder/wwwb.py
@@ -0,0 +1,64 @@
+# Round 1 Housing
+# Maximum 2
+round_1 = [
+    ['public_housing',[4,0,5,-2]],
+    ['low_rise_private_housing',[3,0,-2,0]],
+    ['high_rise_private_housing',[4,0,-1,0]],
+    ['mixed_residential_development',[5,0,3,-3]],
+]
+
+# Round 2 Amenities
+# Maximum 3
+round_2 = [
+    ['playground',[0,2,4,0]],
+    ['park',[-2,4,5,0]],
+    ['library',[-2,0,4,5]],
+    ['community_farm',[-5,2,4,0]],
+    ['skate_park',[0,2,3,0]],
+    ['soccer_court',[0,0,3,2]],
+    ['stadium',[5,-2,5,0]],
+    ['place_of_worship',[-5,0,5,10]],
+    ['museum',[0,-2,0,10]]
+]
+
+# Round 3 Education & Social Development
+# Maximum 2
+round_3 = [
+    ['school',[2,0,5,4]],
+    ['senior_activity_center',[-2,0,3,4]],
+    ['childcare_center',[-2,0,3,4]],
+    ['performing_arts_arena',[5,-2,5,5]],
+    ['cultural_center',[-2,0,0,10]],
+    ['language_school',[-2,0,0,10]]
+]
+
+# Round 4 Entertainment & Commerce
+# Maximum 3
+round_4 = [
+    ['food_center',[3,-2,5,5]],
+    ['cafe_and_bistros',[4,-2,-2,0]],
+    ['nightlife_establishment',[4,-2,2,0]],
+    ['theme_park',[5,-2,5,3]],
+    ['theatre',[0,0,0,10]],
+    ['shopping_mall',[5,-2,2,0]],
+    ['office_building',[5,-2,4,0]],
+    ['factory',[5,-2,5,0]],
+    ['research_facility',[5,5,-2,0]],
+    ['co_working_office_space',[5,5,5,0]],
+]
+
+# Round 5 Utilities & Infrastructures
+# Maximum 5
+round_5 = [
+    ['hospital',[5,0,5,0]],
+    ['polyclinic',[5,0,5,0]],
+    ['army_camp',[4,-2,5,0]],
+    ['police_station',[0,0,3,3]],
+    ['airport',[5,-2,0,0]],
+    ['port',[5,-2,0,0]],
+    ['train_station_network',[10,5,5,0]],
+    ['ev_charging_network',[3,5,-2,0]],
+    ['reservoir',[0,5,-2,5]],
+    ['water_treatment_plant',[5,5,-2,0]],
+    ['columbarium',[-2,0,0,10]],
+]
